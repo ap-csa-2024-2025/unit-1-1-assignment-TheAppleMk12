d2f615aa12a1f3e8837169ff7b216dd3a4f02e0c
Abram Tran <abramhtran@gmail.com>
2024-09-03 11:22:16 -0700

    Done

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,18 @@ public class HelloWorld
 {
   public static void main(String[] args)
   {
+    int myAge;
+    myAge= 19;
+
+    String myName = "Abram Tran";
+
+    double myHeight;
+    myHeight = 5.75;
+
+    boolean inSchool;
+    inSchool = True
+
     System.out.println("Hello world!");
+    System.out.print("My name is " + myName + ", I am " + myAge + " years old. My height is " myHeight + " feet.");
   }
 }
